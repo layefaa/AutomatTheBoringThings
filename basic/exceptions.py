983c0a57c2336c0027f03cd4534e747d5ba5c032
Martin Luther Obahor <kingm4159@gmail.com>
2026-02-05 22:29:11 +0100

    Add basic exception handling example in `exceptions.py`

--- basic/exceptions.py
+++ basic/exceptions.py
@@ -1,4 +1,11 @@
 try:
-    open("nonexistent.txt")
-except Exception:
-    print("Something went wrong!")
+    f = open('just.txt')
+except FileNotFoundError:
+     print("Something went wrong!")
+except Exception as e:
+    print(e)
+else:
+    print(f.read())
+    f.close()
+finally:
+    print("This always runs.")
