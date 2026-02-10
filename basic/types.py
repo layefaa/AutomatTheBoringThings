688d6847182331ef7bf309975229279cd361bd10
Martin Luther Obahor <kingm4159@gmail.com>
2026-02-10 22:57:32 +0100

    Add `types.py` to demonstrate basic usage of lists, tuples, and sets

---
+++ basic/types.py
@@ -0,0 +1,34 @@
+# List
+
+list = [1, 2, 3]
+print(list)
+
+list.append(4)
+
+list.insert(1, 5)
+
+print(list)
+
+# Slices
+print(list[:3])
+
+# Tuples
+
+tuple = (1, 2, 3)
+print(tuple)
+
+# Set
+
+set = {1, 2, 3}
+
+
+# Empty List
+empty_list = []
+empty_list = list()
+
+# Empty Tuple
+empty_tuple = ()
+empty_tuple = tuple()
+
+# Empty Set
+empty_set = set()
